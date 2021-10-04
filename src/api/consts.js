a6a778fcd90c8db879770201ab66cdeecce4df44
on-stu <minsu0523@naver.com>
2021-10-04 13:08:30 +0900

    resolve cors issue

--- src/api/consts.js
+++ src/api/consts.js
@@ -4,8 +4,8 @@
 // export const Auth = "http://localhost:5000/auth";
 // export const Classes = "http://localhost:5000/class";
 
-export const Auth = "http://api.혜윰.com/auth";
-export const Classes = "http://api.혜윰.com/class";
+export const Auth = "https://api.혜윰.com/auth";
+export const Classes = "https://api.혜윰.com/class";
 
 // export const Auth = "/proxy";
 // export const Classes = "/proxy/class";
